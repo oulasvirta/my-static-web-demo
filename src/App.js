1783a6cc66fdf4b5c0b9ed64595f6f341518dcc8
Okko Oulasvirta <okko@oulasvirta.com>
2021-03-19 12:51:04 +0200

    Update app.js - modify greeting AB#2817

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'for Azure demo';
+  return <div>Cowboy coding {value}</div>;
 }
 
 export default App;
